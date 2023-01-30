d3437ea9f849d37b3506c2dae9beff0d771045b3
fodare <folorunsodamilare007@hotmail.com>
2023-01-30 19:57:35 +0100

    Added new column to database

--- FinanceManager/Migrations/20230130182828_TransactionForm.Designer.cs
+++ FinanceManager/Migrations/20230130182828_TransactionForm.Designer.cs
@@ -4,6 +4,7 @@ using FinanceManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinanceManager.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230130182828_TransactionForm")]
+    partial class TransactionForm
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -32,6 +35,12 @@ namespace FinanceManager.Migrations
 
                     b.Property<decimal>("Amount")
                         .HasColumnType("decimal(18, 2)");
+
+                    b.Property<decimal>("TotalSavings")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.Property<decimal>("TotalSpending")
+                        .HasColumnType("decimal(18,2)");
 
                     b.Property<DateTime>("TransactionDate")
                         .HasColumnType("datetime2");
---
+++ FinanceManager/Migrations/20230130182828_TransactionForm.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransactionForm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalSavings",
+                table: "TransactionModels",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalSpending",
+                table: "TransactionModels",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionForm",
+                table: "TransactionModels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalSavings",
+                table: "TransactionModels");
+
+            migrationBuilder.DropColumn(
+                name: "TotalSpending",
+                table: "TransactionModels");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionForm",
+                table: "TransactionModels");
+        }
+    }
+}
