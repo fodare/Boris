17a306b4b360a9679c5fa5c0ecc90200365d22db
fodare <folorunsodamilare007@hotmail.com>
2023-02-04 22:17:37 +0100

    Addded new model to track total savings and spending

---
+++ FinanceManager/Models/Summation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceManager.Models
+{
+    public class Summation
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
+        public decimal TotalSavngs { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
+        public decimal TotalSpending { get; set; }
+
+        [Display(Name = "Last updated")]
+        [DataType(DataType.Date)]
+        public DateTime LastUpdateDate { get; set; }
+    }
+}
