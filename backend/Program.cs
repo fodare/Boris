a29a92ed18dbbd439a1077a41e29dc7917f93056
fodare <folorunsodamilare007@hotmail.com>
2023-12-26 13:35:05 +0100

    Add singleton trasnaction service implementation.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,10 @@
+using backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSingleton<ITransactionService, TransactionService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
