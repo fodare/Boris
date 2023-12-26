53de001cd3411b764d45dd79db9fb04327599a38
fodare <folorunsodamilare007@hotmail.com>
2023-12-26 13:50:18 +0100

    Injected transaction service.

--- backend/Controllers/TransactionController.cs
+++ backend/Controllers/TransactionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Models;
+using backend.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
@@ -11,8 +12,10 @@ namespace backend.Controllers
     [Route("api/v2/[controller]")]
     public class TransactionController : ControllerBase
     {
-        public TransactionController()
+        private readonly ITransactionService _transactionService;
+        public TransactionController(ITransactionService transactionService)
         {
+            _transactionService = transactionService;
         }
 
         [HttpGet("health", Name = "HelathCheck")]
