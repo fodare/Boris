aa94d56f607f2e837969f6ce04735ee1ba9fb8cb
fodare <folorunsodamilare007@hotmail.com>
2023-12-28 14:27:23 +0100

    Add user service singleton.

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddSingleton<ITransactionService, TransactionService>();
+builder.Services.AddSingleton<IUserService, UserService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
