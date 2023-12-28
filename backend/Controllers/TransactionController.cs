dcc9a2886361553f2ac3c48ef91ea5ecae5fbf5d
fodare <folorunsodamilare007@hotmail.com>
2023-12-28 19:40:17 +0100

    Injecting data context to test dapper configuration.

--- backend/Controllers/TransactionController.cs
+++ backend/Controllers/TransactionController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Data;
 using backend.Models;
 using backend.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -13,14 +14,17 @@ namespace backend.Controllers
     public class TransactionController : ControllerBase
     {
         private readonly ITransactionService _transactionService;
+        DataContextDapper _dapper;
         public TransactionController(ITransactionService transactionService)
         {
             _transactionService = transactionService;
+            _dapper = new DataContextDapper();
         }
 
         [HttpGet("health", Name = "HelathCheck")]
         public IActionResult Gethealth()
         {
+            _dapper.ExecuteSql<string>("SELECT GETDATE()");
             return Ok(DateTime.Now);
         }
     }
