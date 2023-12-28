7b993ebd7504c06d9607fa3e778afc0adcad2c2a
fodare <folorunsodamilare007@hotmail.com>
2023-12-28 19:39:19 +0100

    Initial dapper configuration.

---
+++ backend/Data/DataContextDapper.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Globalization;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+
+namespace backend.Data
+{
+    public class DataContextDapper
+    {
+        private readonly string? dbConnectionString = Environment.GetEnvironmentVariable("dbConString");
+        public DataContextDapper()
+        {
+            if (dbConnectionString == null)
+            {
+                throw new NullReferenceException("Db connection string is null. Check the connection string is exported!");
+            }
+        }
+
+        public T ExecuteSql<T>(string sqlCommand)
+        {
+            IDbConnection dbConnection = new SqlConnection(dbConnectionString);
+            var testResponse = dbConnection.QuerySingle<T>(sqlCommand);
+            Console.WriteLine(testResponse);
+            return testResponse;
+        }
+    }
+}
