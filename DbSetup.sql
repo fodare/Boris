23a72b4ec0ed95efeb182a81605c96b8822a3105
fodare <folorunsodamilare007@hotmail.com>
2023-12-28 09:50:47 +0100

    Added create table sql script.

--- DbSetup.sql
+++ DbSetup.sql
@@ -9,3 +9,17 @@ GO
 
 CREATE SCHEMA FinanceManagerSchema;
 GO
+
+CREATE TABLE FinanceManagerSchema.TransactionRecord
+(
+    TransactionId INT IDENTITY(1,1),
+    UserId INT NOT NULL,
+    SpaceId INT NOT NULL,
+    Amount DECIMAL(18, 4),
+    TransactionType VARCHAR(50),
+    TransactionTag VARCHAR(50),
+    Note VARCHAR(255),
+    RecordDate DATETIME,
+    UpdatedDate DATETIME
+);
+GO
