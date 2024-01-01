c96f5ea448e20e34ba1d0d7ddff89914c952cc31
fodare <folorunsodamilare007@hotmail.com>
2024-01-01 20:55:37 +0100

    Add json serialization options

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<ITransactionService, TransactionService>();
 builder.Services.AddSingleton<IUserService, UserService>();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters
+        .Add(new JsonStringEnumConverter())); ;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
