be2a38a4fd7e7c6226c495108308773cdf9e7b91
fodare <folorunsodamilare007@hotmail.com>
2024-01-01 20:55:07 +0100

    Remove space Id  field from transaction_record table.

--- DbSetup.sql
+++ DbSetup.sql
@@ -14,7 +14,6 @@ CREATE TABLE FinanceManagerSchema.TransactionRecord
 (
     TransactionId INT IDENTITY(1,1),
     UserId INT NOT NULL,
-    SpaceId INT NOT NULL,
     Amount DECIMAL(18, 4),
     TransactionType VARCHAR(50),
     TransactionTag VARCHAR(50),
@@ -84,5 +83,29 @@ BEGIN
     ELSE
         THROW 52000,
         'Username exists. Please provide another username.',1;
+END
+GO
+
+CREATE OR ALTER PROCEDURE FinanceManagerSchema.spTransaction_Add
+    /* EXEC FinanceManagerSchema.spTransaction_Add
+        @amount = 20.00, @transactionType = 'Credit',
+        @transactionTag = 'Savings', @note = 'test',
+        @recordDate = '2023-12-31 15:43:49.000'
+    */
+    @userId INT = 0,
+    @amount DECIMAL(18, 4),
+    @transactionType VARCHAR(50),
+    @transactionTag VARCHAR(50),
+    @note VARCHAR(255),
+    @recordDate Datetime
+AS
+BEGIN
+    INSERT INTO FinanceManagerSchema.TransactionRecord
+        (
+        [UserId],[Amount],[TransactionType],[TransactionTag],
+        [Note],[RecordDate]
+        )
+    VALUES
+        (ISNULL(@userId, 0), @amount, @transactionType, @transactionTag, @note, @recordDate)
 END
 GO
