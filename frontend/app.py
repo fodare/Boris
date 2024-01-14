2cb2a35eeb6c4882f3824cb5bc2cab844e5d0a00
fodare <folorunsodamilare007@hotmail.com>
2024-01-14 16:44:33 +0100

    Fetch transaction list from backend api and add eit handler.

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, redirect
 import datetime
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_page():
+    transaction_list = requests.get(
+        "http://localhost:3001/api/v2/Transaction/transactionlists").json()
     current_year = datetime.datetime.now().year
-    return render_template('index.html', current_year=current_year)
+    return render_template('index.html', current_year=current_year, transactionList=transaction_list)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -23,6 +26,12 @@ def login():
 @app.route("/register")
 def register():
     return render_template('register.html')
+
+
+@app.route("/edit/<int:id>")
+def edit(id, methods=['GET', 'POST']):
+    if request.method == 'GET':
+        return "Ok"
 
 
 if __name__ == "__main__":
