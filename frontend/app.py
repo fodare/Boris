57a6c0c70447ff919b4461cfc8aa531280ed4780
fodare <folorunsodamilare007@hotmail.com>
2024-02-03 01:42:29 +0100

    Removed flask login and use session.

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import datetime
 import os
 import requests
@@ -10,33 +10,10 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, log
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
 app.config['SECRET_KEY'] = f'{os.environ.get("secret")}'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(minutes=15)
 host_ip = f'{os.environ.get("host_ip")}'
 backendapi_port = f'{os.environ.get("backend_port", 3001)}'
 BACKEND_API_BASE_URL = f"http://{host_ip}:{backendapi_port}"
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-# ///////////////////// Authentication block ///////////////////// #
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    user_info = get_user_by_id(user_id)
-    return User(user_info)
-
-
-class User(UserMixin):
-    def __init__(self, user_json):
-        self.user_json = user_json
-
-    def get_id(self):
-        try:
-            object_id = self.user_json.get('userId')
-            return int(object_id)
-        except:
-            return None
 
 # ///////////////////// Application routes ///////////////////// #
 
@@ -49,40 +26,41 @@ def home():
 
 @app.route("/record", methods=['GET', 'POST'])
 def record():
-    if request.method == 'GET':
-        transaction_list = requests.get(
-            f"{BACKEND_API_BASE_URL}/api/v2/Transaction/transactionlists", verify=False).json()
-        current_year = datetime.datetime.now().year
-        return render_template('index.html', current_year=current_year,
-                               transactionList=transaction_list)
-    else:
-        request_body = {
-            "amount": f"{request.form['amount']}",
-            "type": f"{request.form['type']}",
-            "tag": f"{request.form['tag']}",
-            "note": f"{request.form['note']}"
-        }
-        response_data = requests.post(
-            f"{BACKEND_API_BASE_URL}/api/v2/Transaction/addtransaction", json=request_body)
-        if response_data.status_code == 200:
-            return redirect(url_for('record'))
+    if 'userName' in session:
+        if request.method == 'GET':
+            transaction_list = requests.get(
+                f"{BACKEND_API_BASE_URL}/api/v2/Transaction/transactionlists", verify=False).json()
+            current_year = datetime.datetime.now().year
+            return render_template('index.html', current_year=current_year,
+                                   transactionList=transaction_list, is_logedIn=True)
         else:
-            flash(
-                "Error recording transaction. Please check you input and try again!", 'error')
-            return redirect(url_for('record'))
+            request_body = {
+                "amount": f"{request.form['amount']}",
+                "type": f"{request.form['type']}",
+                "tag": f"{request.form['tag']}",
+                "note": f"{request.form['note']}"
+            }
+            response_data = requests.post(
+                f"{BACKEND_API_BASE_URL}/api/v2/Transaction/addtransaction", json=request_body)
+            if response_data.status_code == 200:
+                return redirect(url_for('record'))
+            else:
+                flash(
+                    "Error recording transaction. Please check you input and try again!", 'error')
+                return redirect(url_for('record'))
+    return render_template('login.html', is_logedIn=False)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html', current_user=current_user)
+        return render_template('login.html', is_logedIn=False)
     else:
         username = request.form['userName']
         password = request.form['password']
         if check_user_credentials(username, password):
-            user_object = check_user_name(username)
-            user = User(user_object)
-            login_user(user)
+            session['userName'] = username
+            session.permanent = True
             return redirect(url_for('record'))
 
         return redirect(url_for('login'))
@@ -90,7 +68,7 @@ def login():
 
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
-    logout_user()
+    session.pop('userName', None)
     return redirect(url_for('home'))
 
 
@@ -113,60 +91,67 @@ def register():
 
 @app.route("/edit/<int:id>", methods=['GET', 'POST'])
 def edit(id):
-    if request.method == 'GET':
-        response_data = requests.get(
-            f"{BACKEND_API_BASE_URL}/api/v2/Transaction/{id}", verify=False)
-        if response_data.status_code == 200:
-            json_content = response_data.json()
-            return render_template('edit.html', record=json_content["data"])
+    if 'userName' in session:
+        if request.method == 'GET':
+            response_data = requests.get(
+                f"{BACKEND_API_BASE_URL}/api/v2/Transaction/{id}", verify=False)
+            if response_data.status_code == 200:
+                json_content = response_data.json()
+                return render_template('edit.html', record=json_content["data"], is_logedIn=True)
+            else:
+                flash(
+                    f"Error editing record with id {id}. Please try again!", "error")
+                return redirect(url_for("record"))
         else:
-            flash(
-                f"Error editing record with id {id}. Please try again!", "error")
-            return redirect(url_for("record"))
-    else:
-        request_body = {
-            "amount": f"{request.form['amount']}",
-            "transactionType": f"{request.form['type']}",
-            "transactionTag": f"{request.form['tag']}",
-            "note": f"{request.form['note']}"
-        }
-        response_data = requests.put(
-            f"{BACKEND_API_BASE_URL}/api/v2/Transaction/updatetransaction/{id}", json=request_body)
-        if response_data.status_code == 200:
-            return redirect(url_for('record'))
-        else:
-            flash(
-                f"Error editing record with id {id}. Please check your input and try again", "error")
-            return redirect(url_for('edit', id=id))
+            request_body = {
+                "amount": f"{request.form['amount']}",
+                "transactionType": f"{request.form['type']}",
+                "transactionTag": f"{request.form['tag']}",
+                "note": f"{request.form['note']}"
+            }
+            response_data = requests.put(
+                f"{BACKEND_API_BASE_URL}/api/v2/Transaction/updatetransaction/{id}", json=request_body)
+            if response_data.status_code == 200:
+                return redirect(url_for('record'))
+            else:
+                flash(
+                    f"Error editing record with id {id}. Please check your input and try again", "error")
+                return redirect(url_for('edit', id=id))
+    return render_template('login.html', is_logedIn=False)
 
 
 @app.route("/summary", methods=['GET', 'POST'])
 def summary():
-    if request.method == 'GET':
-        return render_template('stats.html')
-    else:
-        request_body = {
-            "startTime": f"{request.form['startDate']}",
-            "endtime": f"{request.form['endDate']}"
-        }
-        respose_data = requests.post(
-            f"{BACKEND_API_BASE_URL}/api/v2/Transaction/summary", verify=False, json=request_body)
-        if respose_data.status_code == 200:
-            json_content = respose_data.json()
-            record_summary = json_content['data']
-            return render_template('stats.html',
-                                   record_summary=record_summary,
-                                   start_date=request.form['startDate'],
-                                   end_date=request.form['endDate']
-                                   )
+    if 'userName' in session:
+        if request.method == 'GET':
+            return render_template('stats.html', is_logedIn=True)
         else:
-            flash("Error retiving transaction summary. Please try again!", "error")
-            return redirect(url_for('summary'))
+            request_body = {
+                "startTime": f"{request.form['startDate']}",
+                "endtime": f"{request.form['endDate']}"
+            }
+            respose_data = requests.post(
+                f"{BACKEND_API_BASE_URL}/api/v2/Transaction/summary", verify=False, json=request_body)
+            if respose_data.status_code == 200:
+                json_content = respose_data.json()
+                record_summary = json_content['data']
+                return render_template('stats.html',
+                                       record_summary=record_summary,
+                                       start_date=request.form['startDate'],
+                                       end_date=request.form['endDate'],
+                                       is_logedIn=True
+                                       )
+            else:
+                flash("Error retiving transaction summary. Please try again!", "error")
+                return redirect(url_for('summary'))
+    return render_template('login.html', is_logedIn=False)
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return render_template('notfound.html')
+def page_neot_found(error):
+    if 'usrName' in session:
+        return render_template('notfound.html', is_logedIn=True)
+    return render_template('notfound.html', is_logedIn=False)
 
 
 if __name__ == "__main__":
