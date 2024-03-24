7750d33649c24d2d8f888278427642cadc943cf4
fodare <folorunsodamilare007@hotmail.com>
2024-03-24 18:48:49 +0100

    Add and implement user service.

--- backend/Services/IUserService.cs
+++ backend/Services/IUserService.cs
@@ -7,13 +7,23 @@ namespace backend.Services
 {
     public interface IUserService
     {
+        bool CreateUser(NewUserDTO newUser);
+
+        UserModel? GetUserByUserName(string userName);
+
+        UserModel? GetUserById(int userId);
+
+        IEnumerable<UserModel>? GetUsers();
+
+        bool LockUser(int userId);
+
+        bool VerifyUser(string userName, string password);
+
         // IEnumerable<UserModel> GetUsers();
 
         // UserModel? GetUser(int userId);
 
         // UserModel? GetUserByUserName(string userName);
-
-        bool CreateUser(NewUserDTO newUser);
 
         // bool VerifyUser(string userName, string password);
     }
--- backend/Services/UserService.cs
+++ backend/Services/UserService.cs
@@ -17,13 +17,12 @@ namespace backend.Services
 
         public bool CreateUser(NewUserDTO newUser)
         {
+            static string getServerTime() => DateTime.Now.ToString("yyyy-MM-dd");
             try
             {
                 var hashedUserPassword = _userHelper.CreatePasswordHash(newUser.UserPassword);
 
-                string sqlCommand = $@"INSERT INTO FinanceRecordSchema.UserRecord
-                    ([UserName], [UserPassword], [IsAdmin], [CreatedDate],[UpdatedDate])
-                    VALUES ( '{newUser.UserName}', '{newUser.UserPassword}', 0, '2023-12-31', '2023-12-31')";
+                string sqlCommand = $@"EXEC FinanceRecordSchema.spUser_Add @userName ='{newUser.UserName}', @userPassword ='{hashedUserPassword}',@isAdmin ={0}, @createDate ='{getServerTime()}', @updateDate = '{getServerTime()}'";
 
                 bool userAdded = _dapper.ExcecuteSqlAdd(sqlCommand);
                 if (userAdded)
@@ -39,68 +38,43 @@ namespace backend.Services
             }
         }
 
-        // public UserModel? GetUser(int userId)
-        // {
-        //     string sqlCommand = @$"EXEC FinanceManagerSchema.spUser_Get 
-        //         @userId = {userId}";
-        //     UserModel qureiedUser = _dapper.ExecuteSql<UserModel>(sqlCommand);
-        //     if (qureiedUser.UserId > 0)
-        //     {
-        //         return qureiedUser;
-        //     }
-        //     Console.WriteLine($"Error feting user with id {userId}");
-        //     return null;
-        // }
+        public UserModel? GetUserByUserName(string userName)
+        {
+            string sqlCommand = @$"EXEC FinanceRecordSchema.spUser_Get @userName = '{userName}'";
+            UserModel qureiedUser = _dapper.ExecuteSql<UserModel>(sqlCommand);
+            if (qureiedUser.UserId > 0)
+            {
+                return qureiedUser;
+            }
+            return null;
+        }
 
-        // public UserModel? GetUserByUserName(string userName)
-        // {
-        //     string sqlCommand = @$"EXEC FinanceManagerSchema.spUser_Get 
-        //         @userName = {userName}";
-        //     UserModel qureiedUser = _dapper.ExecuteSql<UserModel>(sqlCommand);
-        //     if (qureiedUser.UserId > 0)
-        //     {
-        //         return qureiedUser;
-        //     }
-        //     Console.WriteLine($"Error feting user with username {userName}");
-        //     return null;
-        // }
+        public UserModel? GetUserById(int userId)
+        {
+            string sqlCommand = @$"EXEC FinanceRecordSchema.spUser_Get @userId = '{userId}'";
+            UserModel qureiedUser = _dapper.ExecuteSql<UserModel>(sqlCommand);
+            if (qureiedUser.UserId > 0)
+            {
+                return qureiedUser;
+            }
+            return null;
+        }
 
-        // public IEnumerable<UserModel>? GetUsers()
-        // {
-        //     string sqlCommand = $"EXEC FinanceManagerSchema.spUser_Get";
-        //     try
-        //     {
-        //         IEnumerable<UserModel> userList = _dapper.LoadData<UserModel>(sqlCommand);
-        //         return userList;
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         Console.WriteLine($"Error fetching user list. {e.Message}");
-        //         return null;
-        //     }
+        public IEnumerable<UserModel>? GetUsers()
+        {
+            string sqlCommand = "EXEC FinanceRecordSchema.spUser_Get";
+            IEnumerable<UserModel> userList = _dapper.LoadData<UserModel>(sqlCommand);
+            return userList;
+        }
 
-        // }
+        public bool LockUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
 
-        // public bool VerifyUser(string userName, string userPassword)
-        // {
-        //     string sqlCommand = @$"EXEC FinanceManagerSchema.spUser_Get 
-        //         @userName = '{userName}'";
-        //     UserModel qureidUser = _dapper.LoadDataSingle<UserModel>(sqlCommand);
-        //     if (qureidUser.UserName != null)
-        //     {
-        //         bool passwordMatch = _userHelper.VerifyPasswordHash(userPassword, qureidUser.UserPassword);
-        //         if (passwordMatch)
-        //         {
-        //             return true;
-        //         }
-        //         Console.WriteLine($"Error verifying user credentails");
-        //         return false;
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine($"Erro. User account not found!");
-        //         return false;
-        //     }
-        // }
+        public bool VerifyUser(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
