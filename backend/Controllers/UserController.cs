dbddbfd67ed5b329d07b2580b9693cf6702953ba
fodare <folorunsodamilare007@hotmail.com>
2024-03-24 18:47:14 +0100

    Add get user bt id.

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -17,7 +17,7 @@ namespace backend.Controllers
             _userService = userService;
             _mapper = new Mapper(new MapperConfiguration(cfg =>
             {
-                // cfg.CreateMap<UserModel, GetUserDto>().ReverseMap();
+                cfg.CreateMap<UserModel, GetUserDTO>().ReverseMap();
             }));
         }
 
@@ -41,70 +41,30 @@ namespace backend.Controllers
             }
         }
 
-        // [HttpPost("login", Name = ("login"))]
-        // public ActionResult<ResponseModel<string>> AuthenticateUser([FromBody] LoginDto userInfo)
-        // {
-        //     ResponseModel<string> response = new();
-        //     if (_userService.VerifyUser(userInfo.UserName, userInfo.Password))
-        //     {
-        //         response.Success = true;
-        //         response.Message = "User authenticated successfully!";
-        //         return Ok(response);
-        //     }
-        //     else
-        //     {
-        //         response.Success = false;
-        //         response.Message = "Error authenticating user. Please try again!";
-        //         return BadRequest(response);
-        //     }
-        // }
-
-        // [HttpGet("user/{userId}", Name = "GetUserById")]
-        // public ActionResult<ResponseModel<GetUserDto>> GetUser(int userId)
-        // {
-        //     ResponseModel<GetUserDto> response = new();
-        //     var queryResult = _userService.GetUser(userId);
-        //     if (queryResult != null)
-        //     {
-        //         response.Success = true;
-        //         response.Data = _mapper.Map<GetUserDto>(queryResult);
-        //         return Ok(response);
-        //     }
-        //     else
-        //     {
-        //         return BadRequest("Can not find qureied user!");
-        //     }
-        // }
-
-        // [HttpGet("{userName}", Name = "ByUserName")]
-        // public ActionResult<ResponseModel<GetUserDto>> FetchUserByUserName(string userName)
-        // {
-        //     ResponseModel<GetUserDto> response = new();
-        //     var queryResult = _userService.GetUserByUserName(userName);
-        //     if (queryResult != null)
-        //     {
-        //         response.Success = true;
-        //         response.Data = _mapper.Map<GetUserDto>(queryResult);
-        //         return Ok(response);
-        //     }
-        //     else
-        //     {
-        //         response.Success = false;
-        //         response.Message = "Can not find qureied user!";
-        //         return BadRequest(response);
-        //     }
-        // }
-
-        // [HttpGet("users", Name = "GetUserList")]
-        // public ActionResult<ResponseModel<GetUserDto>> FetchUser()
-        // {
-        //     ResponseModel<IEnumerable<GetUserDto>> response = new();
-        //     var userList = _userService.GetUsers();
-        //     IEnumerable<GetUserDto> filteredUserList = _mapper.Map<List<GetUserDto>>(userList);
-        //     response.Message = "Successfully retrived users list";
-        //     response.Success = true;
-        //     response.Data = filteredUserList;
-        //     return Ok(response);
-        // }
+        [HttpGet("{userId}", Name = "GetUserById")]
+        public ActionResult<ResponseModel<GetUserDTO>> GetUser(int userId)
+        {
+            ResponseModel<GetUserDTO> response = new();
+            try
+            {
+                var queryResult = _userService.GetUserById(userId);
+                if (queryResult != null)
+                {
+                    response.Message = "User record retrived successfully!";
+                    response.Data = _mapper.Map<GetUserDTO>(queryResult);
+                    response.Success = true;
+                    return Ok(response);
+                }
+                response.Message = "Error while fetching user. Please try again!";
+                return BadRequest(response);
+            }
+            catch (Exception ex)
+            {
+                response.Message = "Error while fetching user. Please try again!";
+                Console.WriteLine($"Error fetching user with id {userId}. Exception {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, response);
+            }
+        }
     }
+
 }
