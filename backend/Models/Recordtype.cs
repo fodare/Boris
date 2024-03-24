67b2ef60ae3afab975cf848a46704f9482d225d7
fodare <folorunsodamilare007@hotmail.com>
2024-03-24 14:23:39 +0100

    Update transaction type to record type.

--- backend/Models/Recordtype.cs
+++ backend/Models/Recordtype.cs
@@ -1,23 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace backend.Models
 {
     public enum Recordtype
     {
-        Grocery,
-        EatingOut,
-        Rent,
-        Electricity,
-        Radio,
-        Internet,
-        Phone,
-        Extra,
-        Transportation,
-        Books,
-        onlineCourse,
-        Medication,
-        Insurance,
-        Savings,
-        Charity,
-        Loan,
-        Income
+        Credit,
+        Debit
     }
 }
