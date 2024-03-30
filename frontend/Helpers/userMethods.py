b65f7475eeb76cba9b673101ac0f3c7bde153587
fodare <folorunsodamilare007@hotmail.com>
2024-03-30 20:57:57 +0100

    Updated backend API endpoints

--- frontend/Helpers/userMethods.py
+++ frontend/Helpers/userMethods.py
@@ -12,7 +12,7 @@ def check_user_credentials(userName, password):
         "password": f"{password}"
     }
     response_data = requests.post(
-        f"{BACKEND_API_BASE_URL}/api/v2/auth/login", verify=False, json=request_body)
+        f"{BACKEND_API_BASE_URL}/api/v3/User/verify", verify=False, json=request_body)
     if response_data.status_code == 200:
         json_response = response_data.json()
         if json_response['success']:
@@ -24,7 +24,7 @@ def check_user_credentials(userName, password):
 
 def check_user_name(username):
     response_data = requests.get(
-        f"{BACKEND_API_BASE_URL}/api/v2/auth/{username}", verify=False)
+        f"{BACKEND_API_BASE_URL}/api/v3/User/{username}", verify=False)
     if response_data.status_code == 200:
         json_content = response_data.json()
         user_info = json_content['data']
@@ -33,7 +33,7 @@ def check_user_name(username):
 
 def get_user_by_id(user_id):
     response_data = requests.get(
-        f"{BACKEND_API_BASE_URL}/api/v2/auth/user/{user_id}", verify=False)
+        f"{BACKEND_API_BASE_URL}/api/v3/User/userid/{user_id}", verify=False)
     if response_data.status_code == 200:
         json_content = response_data.json()
         user_data = json_content['data']
