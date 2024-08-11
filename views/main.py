f0938c680d71eb968180550c4fc2846ef92dc037
fodare <folorunsodamilare007@hotmail.com>
2024-08-11 13:10:50 +0200

    Added views sub frames.

--- views/main.py
+++ views/main.py
@@ -16,11 +16,14 @@ class App(_tk.Tk):
         self.dbLogic = dbLogic.DBLogic()
 
         # ----------- Sub frames -----------#
-        self.login_frame = LoginView(parent=self)
-        # self.login_frame.pack_propagate(False)
+        self.login_frame = LoginView(self)
+        self.login_frame.grid(row=0, column=0, padx=100, sticky="nsew")
 
         self.register_frame = RegisterView(self)
-        # self.register_frame.pack_propagate(False)
+        self.register_frame.grid(row=0, column=0, padx=100, sticky="nsew")
+
+        self.app_mainFrame = AppMainFrame(self)
+        self.app_mainFrame.grid(row=0, column=0, sticky="nsew")
 
         # ----------- Default view -----------#
         self.login_frame.render_login_frame(self)
@@ -30,7 +33,7 @@ class App(_tk.Tk):
 class LoginView(_tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        self.grid(row=0, column=0, padx=100, sticky="nsew")
+        # self.grid(row=0, column=0, padx=100, sticky="nsew")
 
     def render_login_frame(self, parent):
 
@@ -52,6 +55,7 @@ class LoginView(_tk.Frame):
             else:
                 messagebox.showinfo(
                     title="Success!", message="Successfully logged in!")
+                parent.app_mainFrame.render_app_main_frame(parent)
 
         # ------------ Widgets ------------ #
         self.view_label = _tk.Label(self, text="Login", font=(
@@ -80,7 +84,7 @@ class LoginView(_tk.Frame):
 class RegisterView(_tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        self.grid(row=0, column=0, padx=100, sticky="nsew")
+        # self.grid(row=0, column=0, padx=100, sticky="nsew")
 
     def render_register_frame(self, parent):
 
@@ -125,6 +129,133 @@ class RegisterView(_tk.Frame):
         self.tkraise()
 
 
+# ------------- App services  ------------- #
+
+
+class AppMainFrame(_tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def render_app_main_frame(self, parent):
+        self.options_menu_frame = OptionsMenuFrame(self)
+        self.options_menu_frame.pack()
+        self.options_menu_frame.pack_propagate(False)
+        self.options_menu_frame.configure(width=600, height=38)
+        # self.options_menu_frame.configure(width=600, height=38,  bg="green")
+
+        self.content_frame = ContentFrame(self)
+        self.content_frame.pack(fill=_tk.BOTH, expand=True)
+
+        self.tkraise()
+
+# ------------- App services subframe  ------------- #
+
+
 class OptionsMenuFrame(_tk.Frame):
     def __init__(self, parent):
-        super().__init(parent)
+        super().__init__(parent)
+
+        def set_options_button_active(name):
+            for child in self.winfo_children():
+                if name == child.winfo_name():
+                    child.configure(background="gray",
+                                    highlightthickness=4)
+                else:
+                    child.configure(background="#d9d9d9", highlightthickness=0)
+
+        def handle_home_view(name):
+            set_options_button_active(name)
+            parent.content_frame.render_home_view()
+
+        def handle_password_view(name):
+            set_options_button_active(name)
+            parent.content_frame.render_password_view()
+
+        def handle_finance_view(name):
+            set_options_button_active(name)
+            parent.content_frame.render_finance_view()
+
+        self.home_button = _tk.Button(
+            self, text="Home", name="home", activebackground="gray", command=lambda: handle_home_view("home")).place(x=0, y=0, width=125)
+        self.password_button = _tk.Button(
+            self, text="Passwords", name="password", activebackground="gray", command=lambda: handle_password_view("password")).place(x=125, y=0, width=125)
+        self.finance_button = _tk.Button(
+            self, text="Finances", name="finance", activebackground="gray", command=lambda: handle_finance_view("finance")).place(x=250, y=0, width=125)
+
+
+class ContentFrame(_tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.home_frame = HomeView(self)
+        self.password_frame = PasswordView(self)
+        self.finance_frame = FinanceView(self)
+
+        self.render_home_view()
+
+    def render_home_view(self):
+        self.home_frame.render_home_contnet()
+
+    def render_password_view(self):
+        self.password_frame.render_password_contnet()
+
+    def render_finance_view(self):
+        self.finance_frame.render_finance_contnet()
+
+# ------------- Views / Services  ------------- #
+
+
+class HomeView(_tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid(row=0, column=0, sticky="nsew")
+
+    def render_home_contnet(self):
+        _tk.Label(self, text="This is the home View!").grid(
+            row=0, column=0)
+        # _tk.Label(self, text="This is the home view message!").grid(row = 1, column = 0)
+        self.tkraise()
+
+
+class PasswordView(_tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid(row=0, column=0, sticky="nsew")
+
+    def render_password_contnet(self):
+        _tk.Label(self, text="This is the password View!").grid(
+            row=0, column=0)
+        # _tk.Label(self, text="This is the password password message!").grid(row=1, column=0)
+        self.tkraise()
+
+
+class FinanceView(_tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid(row=0, column=0, sticky="nsew")
+
+    def render_finance_contnet(self):
+        _tk.Label(self, text="This is the finance view!").grid(row=0, column=0)
+        # _tk.Label(self, text="This is the password view message!").grid(row=1, column=0)
+        self.tkraise()
+
+
+class NewsView(_tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid(row=0, column=0, sticky="nsew")
+
+    def render_news_contnet(self):
+        _tk.Label(self, text="This is the news view!").grid(row=0, column=0)
+        # _tk.Label(self, text="This is the news view message!").grid(row=1, column=0)
+        self.tkraise()
+
+
+class GoalsView(_tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid(row=0, column=0, sticky="nsew")
+
+    def render_goals_content(self):
+        _tk.Label(self, text="This is the goals view!").grid(row=0, column=0)
+        # _tk.Label(self, text="This is the goals view message!").grid(row=1, column=0)
+        self.tkraise()
