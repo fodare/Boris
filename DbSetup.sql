be8ab77f6b86cc3647752f01164b4ae9deb25412
fodare <folorunsodamilare007@hotmail.com>
2024-08-30 23:31:15 +0200

    Add new transactions db schema.

--- DbSetup.sql
+++ DbSetup.sql
@@ -7,6 +7,7 @@ GO
 USE Boris;
 GO
 
+-------------------- User Account Schema --------------------
 CREATE SCHEMA UserSchema;
 GO
 
@@ -169,5 +170,76 @@ BEGIN
     DELETE FROM PasswordSchema.Passwords 
     OUTPUT DELETED.id
     WHERE Passwords.id = ISNULL(@id, id)
+END
+GO
+
+--------------------  Transaction Schema --------------------
+
+CREATE SCHEMA TransactionRecordSchema;
+GO
+
+CREATE TABLE TransactionRecordSchema.Transactions
+(
+    id INT IDENTITY(1,1),
+    Amount DECIMAL(18,4),
+    TransactionType VARCHAR(10),
+    TransactionTag VARCHAR(50),
+    TransactionNote VARCHAR(255),
+    CreateDate DATE,
+    UpdateDate DATE
+);
+GO
+
+CREATE CLUSTERED INDEX cix_Transactions_TransactionId
+    ON TransactionRecordSchema.Transactions (id);
+GO
+
+CREATE OR ALTER PROCEDURE TransactionRecordSchema.spTransactionsAdd
+    /* 
+    EXEC TransactionRecordSchema.spTransactionsAdd
+        @amount =, @type = 'Credict',
+        @tag = 'Savings', @note = 'test',
+        @createDate = '2024-08-30', @updateDate='2024-08-30'
+    */
+    @amount DECIMAL(18, 4),
+    @type VARCHAR(10),
+    @tag VARCHAR(50),
+    @note VARCHAR(255),
+    @createDate DATE,
+    @updateDate DATE
+AS
+BEGIN
+    INSERT INTO TransactionRecordSchema.Transactions
+        (
+        Amount,TransactionType,TransactionTag,TransactionNote,CreateDate,UpdateDate
+        )
+    VALUES
+        (@amount, @type, @tag, @note, @createDate, @updateDate)
+END
+GO
+
+CREATE OR ALTER PROCEDURE TransactionRecordSchema.spTransactions_Get
+    /* 
+    EXEC TransactionRecordSchema.spTransactions_Get @id=4
+    EXEC TransactionRecordSchema.spTransactions_Get @startDate = '2024-08-01', @endDate = '2024-08-02'
+    */
+    @id INT = null,
+    @startDate DATE = null,
+    @endDate DATE =  NULL
+AS
+BEGIN
+    IF @startDate is NOT NULL
+    BEGIN
+        SELECT *
+        FROM TransactionRecordSchema.Transactions WITH(NOLOCK)
+        WHERE CreateDate >= ISNULL(@startDate, CreateDate)
+            AND CreateDate <= ISNULL(@endDate,CreateDate)
+        ORDER By CreateDate DESC
+    END
+    ELSE
+        SELECT *
+    FROM TransactionRecordSchema.Transactions WITH(NOLOCK)
+    WHERE id = ISNULL(@id , id)
+    ORDER By CreateDate DESC
 END
 GO
